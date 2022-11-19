456b0fd3283c9d554d051f4e4588efc57463ecfe
Marek Wal <walmarek1987@gmail.com>
2022-11-19 17:32:37 +0100

    compleated & delete buttons added to js

---
+++ js/script.js
@@ -0,0 +1,89 @@
+console.log("Hi everyone");
+
+{
+  const tasks = [
+    {
+      content: "create project",
+      done: true,
+    },
+    {
+      content: "do homework",
+      done: false,
+    },
+  ];
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+  };
+
+  const bindEvents = () => {
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index);
+      });
+    });
+
+    const taggleDoneButtons = document.querySelectorAll(".js-done");
+
+    taggleDoneButtons.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+      });
+    });
+  };
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+       <li ${task.done ? 'style="text-decoration: line-through"' : ""}>
+       <button class="js-done">compleated</button>
+       <button class="js-remove">Delete</button>
+         ${task.content}
+       </li>
+      `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    bindEvents();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-form-task").value.trim();
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  };
+
+  const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+  };
+
+  init();
+}
